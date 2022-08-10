b6a8a7c1c763ae032afe7e83729f3a7af0449289
ttpf-P <65406575+ttpf-P@users.noreply.github.com>
2022-08-10 21:30:02 +0200

    add testing projection_shader

--- core/geometry.cuh
+++ core/geometry.cuh
@@ -3,7 +3,21 @@
 
 #include "CUcommons.hpp"
 
-__global__ void projection_shader(point3 *vertices){
+__global__ void projection_shader(size_t vertex_num, point3 *vertices_3d, point3 *vertices_projected,
+                                  point3 camera_center, vec3 screen_unit_normal, double screen_dist_origin){
+    unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int stride = blockDim.x * gridDim.x;
+    for (unsigned int i = index; i < vertex_num; i += stride){
+        vec3 unit_direction = unit_vector(camera_center - vertices_3d[i]);
+        double s = sum(screen_unit_normal * unit_direction);
+        if (s != 0){ //TODO: check if branching at the end and ignoring zero division is faster
+            double dist = (screen_dist_origin - sum(screen_unit_normal*vertices_3d[i])) / s;
+            if (dist > 0){
+                vec3 intersect = vertices_3d[i] + unit_direction*dist;
+                vertices_projected[i] = vec3(intersect.x(), intersect.y(), dist); //TODO: implement camera rotation
+            }
+        }
+    }
 
 }
 
--- main.cu
+++ main.cu
@@ -3,6 +3,7 @@
 #include "CUcommons.hpp"
 
 #include "core/geometry.cuh"
+#include "classes/triangle.cuh"
 
 
 __global__ void main_kernel(double *state){
@@ -16,18 +17,44 @@ __global__ void main_kernel(double *state){
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << boost::stacktrace::stacktrace() << std::endl;
-    double *test_state;
 
-    checkCudaErrors(cudaMallocManaged(&test_state, sizeof(double)));
-    *test_state = 2;
+    //-------------------------geometry stage-------------------------
+    point3 *vertex_buf_old;
+    point3 *vertex_buf_new;
 
-    main_kernel<<<1,1>>>(test_state);
+    size_t vertex_num = 3;
+
+    checkCudaErrors(cudaMallocManaged(&vertex_buf_old, sizeof(point3)*vertex_num));
+    checkCudaErrors(cudaMallocManaged(&vertex_buf_new, sizeof(point3)*vertex_num));
+
+
+    for (int i = 0; i < 3; ++i){
+        vertex_buf_old[i] = point3::random()+point3(0,0,1);
+    }
+
+    std::cout << "initial vertex buffer" << std::endl;
+    std::cout << vertex_buf_old[0] << std::endl;
+    std::cout << vertex_buf_old[1] << std::endl;
+    std::cout << vertex_buf_old[2] << std::endl;
+
+    projection_shader<<<1,3>>>(vertex_num, vertex_buf_old, vertex_buf_new,
+                               point3(0,0,0), point3(0,0,1), 1);
 
     cudaDeviceSynchronize();
 
-    std::cout << *test_state << std::endl;
+    std::cout << "final vertex buffer";
+    std::cout << vertex_buf_new[0] << std::endl;
+    std::cout << vertex_buf_new[1] << std::endl;
+    std::cout << vertex_buf_new[2] << std::endl;
 
-    cudaFree(test_state);
+    cudaFree(vertex_buf_old);
+
+    //-------------------------rasterization stage-------------------------
+
+    cudaFree(vertex_buf_new);
+
+
+
     cudaDeviceReset();
 
     return 0;
